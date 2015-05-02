f569f2859f511c321dea6fca87f8abd72b11495e
Liu Xinan <xinan@me.com>
2015-05-02 23:56:00 +0800

    Added simple interfaces

---
+++ RemoteControlCar/RemoteControlCar/ButtonControlViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ButtonControlViewController.swift
+//  RemoteControlCar
+//
+//  Created by Liu Xinan on 2/5/15.
+//  Copyright (c) 2015 Liu Hongnan. All rights reserved.
+//
+
+import UIKit
+
+class ButtonControlViewController: UIViewController {
+
+
+    @IBAction func forward(sender: UIButton) {
+        println("Going forward")
+    }
+    
+    @IBAction func backward(sender: UIButton) {
+        println("Going backward")
+    }
+    
+    @IBAction func turnLeft(sender: UIButton) {
+        println("Turning left")
+    }
+    
+    @IBAction func turnRight(sender: UIButton) {
+        println("Turning right")
+    }
+    
+    @IBAction func stop(sender: UIButton) {
+        println("Stopped")
+    }
+    
+
+}
---
+++ RemoteControlCar/RemoteControlCar/GyroControlViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GyroControlViewController.swift
+//  RemoteControlCar
+//
+//  Created by Liu Xinan on 2/5/15.
+//  Copyright (c) 2015 Liu Hongnan. All rights reserved.
+//
+
+import UIKit
+
+class GyroControlViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
